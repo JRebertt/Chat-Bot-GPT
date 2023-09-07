a9e1ee83519e7e09177c484199cc6a08b2aa4eac
JRebertt <joaorebertt0219@gmail.com>
2023-09-07 01:04:59 -0300

    Configurado OpenAI e ajustes no chatbot

--- src/bot.ts
+++ src/bot.ts
@@ -1,25 +1,45 @@
-import * as venom from 'venom-bot'
+import * as venom from 'venom-bot';
+import OpenAI from "openai";
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+const openai = new OpenAI({apiKey:process.env.OPENAI_API_KEY });
+
 
 venom
   .create({
-    session: 'session-name' //name of session
+    session: 'Test-GPT' 
   })
-  .then((client: any) => start(client))
-  .catch((erro: any) => {
+  .then(client => start(client))
+  .catch(erro => {
     console.log(erro);
   });
 
-function start(client: any) {
-  client.onMessage((message: { body: string; isGroupMsg: boolean; from: any; }) => {
-    if (message.body === 'Hi' && message.isGroupMsg === false) {
-      client
-        .sendText(message.from, 'Welcome Venom 🕷')
-        .then((result: any) => {
-          console.log('Result: ', result); //return object success
-        })
-        .catch((erro: any) => {
-          console.error('Error when sending: ', erro); //return object error
+  function start(client: any) {
+    client.onMessage(async (message: { body: string; isGroupMsg: boolean; from: any; }) => {
+      if (message.isGroupMsg === false) {  // Se a mensagem não é de um grupo
+        const completion = await openai.chat.completions.create({
+          messages: [
+            { role: "system", content: "You are a helpful assistant." },
+            { role: "user", content: message.body }  // Usando a mensagem recebida como input
+          ],
+          model: "gpt-3.5-turbo",
+          temperature: 0,
+          max_tokens: 256
         });
-    }
-  });
-}
+  
+        const response = completion.choices[0].message.content;  // Obtendo a resposta do OpenAI
+  
+        client.sendText(message.from, response)  // Enviando a resposta do OpenAI de volta ao usuário do WhatsApp
+          .then((result: any) => {
+            console.log('Result: ', result);
+          })
+          .catch((erro: any) => {
+            console.error('Error when sending: ', erro);
+          });
+      }
+    });
+  }
---
+++ src/lib/openai.ts
@@ -0,0 +1,10 @@
+// const { Configuration, OpenAIApi } = require("openai");
+
+// import { config } from "../config/config"
+
+
+// const configuration = new Configuration({
+//   apiKey: config.openAI.apiToken,
+// })
+
+// export const openai = new OpenAIApi(configuration)
